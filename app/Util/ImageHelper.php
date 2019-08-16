71d07ed459c4fad1303519f7734511c0fd02ecd1
Ronald Bunk <me@azoraqua.com>
2019-08-16 05:14:00 +0200

    Added helper.
    Signed-off-by: Ronald Bunk <me@azoraqua.com>

--- app/Util/ImageHelper.php
+++ app/Util/ImageHelper.php
@@ -4,12 +4,15 @@
 namespace AQ_Blog\Util;
 
 
+use AQ_Blog\Role;
 use AQ_Blog\User;
+use Illuminate\Support\Str;
 
 class ImageHelper
 {
     private const STORAGE_DIR = 'storage/';
     private const AVATAR_DIR = self::STORAGE_DIR . 'avatars/';
+    private const BADGE_DIR = self::STORAGE_DIR . 'badges/';
 
     private const ONLINE_STATUS = self::STORAGE_DIR . 'online_status.png';
     private const OFFLINE_STATUS = self::STORAGE_DIR . 'offline_status.png';
@@ -22,5 +25,22 @@ class ImageHelper
     public static function getUserStatus(User $user): string
     {
         return asset((self::STORAGE_DIR . $user->online()) ? self::ONLINE_STATUS : self::OFFLINE_STATUS);
+    }
+
+    public static function getRoleBadge(Role $role): string
+    {
+        $badge = $role->badge;
+
+        if (Str::startsWith($badge, '#')) {
+            return $badge; // Hex color code.
+        }
+
+        $url = asset(self::BADGE_DIR . $role->badge);
+
+        if (isset($url) && !empty($url)) {
+            return $url; // Badge image.
+        }
+
+        return Role::DEFAULT_BADGE_COLOR; // If not exists, fallback to default.
     }
 }
