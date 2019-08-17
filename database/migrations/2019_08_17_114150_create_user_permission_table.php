7e26de1f1ffebf0064dc6d7d87b1e1c9429ddd42
Ronald Bunk <me@azoraqua.com>
2019-08-17 17:40:56 +0200

    Updated.
    Signed-off-by: Ronald Bunk <me@azoraqua.com>

---
+++ database/migrations/2019_08_17_114150_create_user_permission_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserPermissionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        //
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        //
+    }
+}
---
+++ database/migrations/2019_08_17_114201_create_role_permission_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRolePermissionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        //
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        //
+    }
+}
